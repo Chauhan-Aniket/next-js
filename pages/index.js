f48c4f8e53ca83592990c34892edf8b7a6cd914d
Chauhan-Aniket <aniketchauhan2125@gmail.com>
2023-04-14 13:59:20 +0530

    feat: Link component navigation

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
+import Link from "next/link";
+
 const Home = () => {
 	return (
 		<>
 			<h1>Home Page</h1>
+			<Link href="/about">About</Link>
+			<br />
+			<Link href="/blog">Blog</Link>
+			<br />
+			<Link href="/product">Product</Link>
 		</>
 	);
 };
--- pages/product/index.js
+++ pages/product/index.js
@@ -1,9 +1,24 @@
-const ProductList = () => {
+import Link from "next/link";
+
+const ProductList = ({ productId = 100 }) => {
 	return (
 		<>
-			<h1>Product 1</h1>
-			<h1>Product 2</h1>
-			<h1>Product 3</h1>
+			<Link href="/">Go to Home</Link>
+			<h1>
+				<Link href="product/1">Product 1</Link>
+			</h1>
+			<h1>
+				<Link href="product/2">Product 2</Link>
+			</h1>
+			<h1>
+				<Link href="product/3">Product 3</Link>
+			</h1>
+			<h1>
+				{/* replace prop will replace current history state instead of adding new url */}
+				<Link href={`product/${productId}`} replace>
+					Product {productId}
+				</Link>
+			</h1>
 		</>
 	);
 };
