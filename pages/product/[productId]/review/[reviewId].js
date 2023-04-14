cc07340d068b96dd73cf044ea2f687f7b7e97812
Chauhan-Aniket <aniketchauhan2125@gmail.com>
2023-04-14 12:25:10 +0530

    feat: nested dynamic routes

---
+++ pages/product/[productId]/review/[reviewId].js
@@ -0,0 +1,16 @@
+import { useRouter } from "next/router";
+
+const ReviewList = () => {
+	const router = useRouter();
+	const { productId, reviewId } = router.query;
+
+	return (
+		<>
+			<h1>
+				Review {reviewId} for Product {productId}
+			</h1>
+		</>
+	);
+};
+
+export default ReviewList;
